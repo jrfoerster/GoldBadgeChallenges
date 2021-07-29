ccc01346e792b43569c210fc5f0d1c483c3aa2c8
Justin Foerster <jrfoerster@gmail.com>
2021-07-29 19:36:27 -0400

    initial commit for 02_Claims

---
+++ 02_Claims.ConsoleUI/ProgramUI.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Claims.ConsoleUI
+{
+    public class ProgramUI
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ 02_Claims.Repository/Claim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Claims.Repository
+{
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+        public ClaimType Type { get; set; }
+        public string Description { get; set; }
+        public decimal ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid => (DateOfClaim - DateOfIncident).Days <= 30;
+    }
+}
