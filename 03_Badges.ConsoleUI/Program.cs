194aeb639707a55fe86fa860e6c4f322e225b183
Justin Foerster <jrfoerster@gmail.com>
2021-08-03 19:07:25 -0400

    03_Badges initial commit

---
+++ 03_Badges.ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Badges.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new ProgramUI();
+            program.Run();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 03_Badges.ConsoleUI/ProgramUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Badges.ConsoleUI
+{
+    class ProgramUI
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
