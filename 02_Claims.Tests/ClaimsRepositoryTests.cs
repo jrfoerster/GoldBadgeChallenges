3b2fb270e6e7a67e9b46348926fd0134d4421319
Justin Foerster <jrfoerster@gmail.com>
2021-08-05 18:28:24 -0400

    ClaimsRepositoryTests added GetAll() test

--- 02_Claims.Tests/ClaimsRepositoryTests.cs
+++ 02_Claims.Tests/ClaimsRepositoryTests.cs
@@ -1,6 +1,8 @@
 ﻿using _02_Claims.Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02_Claims.Tests
 {
@@ -44,6 +46,26 @@ namespace _02_Claims.Tests
             repository.Add(expected);
             var actual = repository.HandleNextClaim();
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ClaimsRepository_GetAll_AreEqual()
+        {
+            var expected = new Claim[]
+            {
+                new Claim(),
+                new Claim(),
+                new Claim()
+            };
+
+            var repository = new ClaimsRepository();
+            foreach (var claim in expected)
+            {
+                repository.Add(claim);
+            }
+            List<Claim> actual = repository.GetAll().ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
